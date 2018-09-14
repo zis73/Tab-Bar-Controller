89ab6868e47da8ca925000a2a7f67a6654f72d15
zis <desert73eagle@gmail.com>
2018-09-14 15:01:55 +0300

    with code

---
+++ Tab Bar Controller/DarkViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DarkViewController.swift
+//  Tab Bar Controller
+//
+//  Created by student on 14.09.2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    let name = "Biohazard"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        text += "\(name) - \(#function)\n"
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        text += "\(name) - \(#function)\n"
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        text += "\(name) - \(#function)\n"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        text += "\(name) - \(#function)\n"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        text += "\(name) - \(#function)\n"
+        textView.text = text
+    }
+    
+}
